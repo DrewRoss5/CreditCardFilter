c41cbe66cb3514a7f970cadbd6a1ed7bc4ddf718
DrewRoss5 <145530440+DrewRoss5@users.noreply.github.com>
2024-01-27 11:16:40 -0700

    Move digit parsing to its own function

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,22 @@ int get_digit(char digit){
         return -1;
     }
 }
+
+int parse_num(std::vector<int>& digits, std::string number_str){
+    int digit;
+    for (char i : number_str){
+        // skip dashses and spaces
+        if (i != ' ' && i != '-'){
+            digit = get_digit(i);
+            if (digit != -1)
+                digits.push_back(digit);
+            else
+                return -1;
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char** argv){
     if (argc != 3){
         std::cerr << "Error: This program accepts exactly two arguments." << std::endl;
@@ -29,17 +45,12 @@ int main(int argc, char** argv){
     int digit;
     // check if each card number is valid, and write it to the output file if so
     while(std::getline(in, tmp)){
-        // create a vector representing the digits in the card
+        // read the card number and save the result
         digits.clear();
-        for (char i : tmp){
-            digit = get_digit(i);
-            // skip invalid numbers
-            if (digit != -1)
-                digits.push_back(digit);
-        }
-        // check if the card number is valid, and write it to the output file if so
-        if (luhn_valid(digits))
-            out << tmp << "\n";
+        int parsed = parse_num(digits, tmp);
+        // write the card number to the file if it's valid.
+        if (parsed == 0 && luhn_valid(digits))
+                  out << tmp << '\n';
     }
     in.close();
     out.close();
